4afcb35826b91cb0ace1ff50a48c08a6a0efbb6a
noseGoblin <feedjaypie@gmail.com>
2018-12-09 23:58:45 -0800

    added jsonplaceholder API content in action creator fetchPosts with some examples of bad Redux code practice that breaks Redux Thunk

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {
-  connect
-} from 'react-redux';
-import {
-  fetchPosts
-} from '../actions';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
-    return <div > Post List < /div>;
+    return <div>Post List</div>;
   }
 }
 
-export default connect(null, {
-  fetchPosts
-})(PostList);
+export default connect(null, { fetchPosts })(PostList);
