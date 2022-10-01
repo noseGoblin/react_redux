b5c0375ee1961d477ffff7e4027549a724a5011e
Jacob Mompean <jaymompean@gmail.com>
2022-10-01 16:19:35 -0500

    add blog post list

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,15 +2,14 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // BEFORE REFACTOR //
 //  export const fetchPosts = () => {
-  // return async function(dispatch, getState) {
-  //   const response = await jsonPlaceholder.get('/posts');
+// return async function(dispatch, getState) {
+//   const response = await jsonPlaceholder.get('/posts');
 
-  //   dispatch({ type: 'FETCH_POSTS', payload: response })
-  // };
+//   dispatch({ type: 'FETCH_POSTS', payload: response })
+// };
 
-export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
-
