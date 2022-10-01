ae916051a7aee50d788b41a44214745af036cfcd
Jacob Mompean <jaymompean@gmail.com>
2022-10-01 17:39:54 -0500

    refactor to make users refetchable and eliminate extra fetch requests (remove memoize)

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,13 +1,13 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-// BEFORE REFACTOR //
-//  export const fetchPosts = () => {
-// return async function(dispatch, getState) {
-//   const response = await jsonPlaceholder.get('/posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
 
-//   dispatch({ type: 'FETCH_POSTS', payload: response })
-// };
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
@@ -15,9 +15,16 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/Users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+// ** memoized solution below **
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/Users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
