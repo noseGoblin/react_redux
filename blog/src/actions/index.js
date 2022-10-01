1fd91190c479d3d535997f27856d95b02d65a0bc
Jacob Mompean <jaymompean@gmail.com>
2022-10-01 17:10:33 -0500

    correctly memoize'd fetch users

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -15,8 +15,12 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/Users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
