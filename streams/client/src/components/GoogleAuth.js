46c3e46c8400bd81b264312be3b289e1de7fb061
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 11:46:23 -0500

    add google auth button

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,19 +1,36 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '874632318331-se56v2cjd1v00njv2jha73j8bdfb3nkk.apps.googleusercontent.com',
-        scope: 'email',
-        plugin_name: 'Streamy',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '874632318331-se56v2cjd1v00njv2jha73j8bdfb3nkk.apps.googleusercontent.com',
+          scope: 'email',
+          plugin_name: 'Streamy',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in...</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
