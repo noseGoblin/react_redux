5bf97f73cf3727317994cefdeb51674e25ced9da
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 14:00:10 -0500

    refactor to add language store to app

--- translate/src/App.js
+++ translate/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import UserCreate from './components/UserCreate';
-import LanguageContext from './context/LanguageContext';
+import { LanguageStore } from './context/LanguageContext';
 import ColorContext from './context/ColorContext';
 import LangugeSelector from './components/LanguageSelector';
 
 class App extends React.Component {
-  state = { language: 'english' };
-
-  onLanguageChange = (language) => {
-    this.setState({ language: language });
-  };
-
   render() {
     return (
       <div className='ui container'>
-        <LangugeSelector onLanguageChange={this.onLanguageChange} />
-        <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LangugeSelector />
           <ColorContext.Provider value='primary'>
             <UserCreate />
           </ColorContext.Provider>
-        </LanguageContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
